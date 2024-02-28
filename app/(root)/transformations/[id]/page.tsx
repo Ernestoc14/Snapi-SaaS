1a7c3c846a14d4baad6f079675f56dedb408750b
Ernestoc14 <descreed34@hotmail.com>
2024-02-27 21:07:33 -0500

    Feat: Adding Routing and Folder Structure

---
+++ app/(root)/transformations/[id]/page.tsx
@@ -0,0 +1,15 @@
+import React from 'react'
+
+const TransformationsPage = () => {
+  return (
+    <div>TransformationsPage</div>
+  )
+}
+
+export default TransformationsPage
+
+// localhost:3000/transformations
+// Static Routes because the page will always be the same (Not gonna use it here)
+
+// localhost:3000/transformations/[id]
+// Dynamic Routes becauuse each Transformation will have its own id

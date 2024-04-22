d8a45e7517ef7a3cb5256de890dc9867e0151d6c
Ernestoc14 <descreed34@hotmail.com>
2024-04-21 22:12:19 -0500

    Feat: Adding a Delete Image Server Action

--- lib/actions/image.actions.ts
+++ lib/actions/image.actions.ts
@@ -5,6 +5,7 @@ import { connectToDatabase } from "../database/mongoose"
 import { handleError } from "../utils"
 import User from "../database/models/user.model"
 import Image from "../database/models/image.model"
+import { redirect } from "next/navigation"
 
 // file containing server actions for image manipulation
 
@@ -29,8 +30,18 @@ export async function addImage({ image, userId, path }: AddImageParams) {
 export async function updateImage({ image, userId, path }: UpdateImageParams) {
   try {
     await connectToDatabase()
+
+    const imageToUpdate = await Image.findById(image._id)
+
+    if (!imageToUpdate || imageToUpdate.author.toHexString() !== userId) throw new Error("Image not found or Unauthorized")
+
+    const updateImage = await Image.findByIdAndUpdate(
+      imageToUpdate._id,
+      image,
+      { new: true }
+    )
     revalidatePath(path)
-    return JSON.parse(JSON.stringify(image))
+    return JSON.parse(JSON.stringify(updateImage))
   } catch (error) {
     handleError(error)
   }
@@ -40,10 +51,13 @@ export async function updateImage({ image, userId, path }: UpdateImageParams) {
 export async function deleteImage(imageId: string) {
   try {
     await connectToDatabase()
-    revalidatePath(path)
-    return JSON.parse(JSON.stringify(image))
+
+    await Image.findByIdAndDelete(imageId)
+
   } catch (error) {
     handleError(error)
+  } finally {
+    redirect('/')
   }
 }
 

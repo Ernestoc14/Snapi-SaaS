6b720e04e00884d3e12a53e500427113ed1416c8
Ernestoc14 <descreed34@hotmail.com>
2024-05-18 22:47:05 -0500

    Feat: Add Collection Component to render images, add pagination and a new server action to get all the images

--- app/(root)/page.tsx
+++ app/(root)/page.tsx
@@ -1,8 +1,15 @@
+import { Collection } from "@/components/shared/Collection";
 import { navLinks } from "@/constants";
+import { getAllImages } from "@/lib/actions/image.actions";
 import Image from "next/image";
 import Link from "next/link";
 
-const Home = () => {
+const Home = async ({ searchParams }: SearchParamProps) => {
+  const page = Number(searchParams?.page || 1);
+  const searchQuery = (searchParams?.query as string) || "";
+
+  const images = await getAllImages({ page, searchQuery });
+
   return (
     <>
       <section className="home">
@@ -26,6 +33,15 @@ const Home = () => {
             </Link>
           ))}
         </ul>
+      </section>
+
+      <section className="sm:mt-12">
+        <Collection
+          hasSearch={true}
+          images={images?.data}
+          totalPages={images?.totalPage}
+          page={page}
+        />
       </section>
     </>
   );
--- lib/actions/image.actions.ts
+++ lib/actions/image.actions.ts
@@ -7,6 +7,8 @@ import User from "../database/models/user.model"
 import Image from "../database/models/image.model"
 import { redirect } from "next/navigation"
 
+import { v2 as cloudinary } from "cloudinary"
+
 const populateUser = (query: any) => query.populate({ path: "author", model: User, select: "_id firstName lastName" })
 
 // file containing server actions for image manipulation
@@ -70,9 +72,54 @@ export async function getImageById(imageId: string) {
 
     const image = await populateUser(Image.findById(imageId))
 
-    if(!image) throw new Error("Image not found")
-    
+    if (!image) throw new Error("Image not found")
+
     return JSON.parse(JSON.stringify(image))
+  } catch (error) {
+    handleError(error)
+  }
+}
+
+// GET IMAGES
+export async function getAllImages({ page = 1, limit = 9, searchQuery = "" }: { limit?: number; page?: number; searchQuery?: string; }) {
+  try {
+    await connectToDatabase()
+    cloudinary.config({
+      cloud_name: process.env.NEXT_PUBLIC_CLOUDINARY_CLOUD_NAME,
+      api_key: process.env.CLOUDINARY_API_KEY,
+      api_secret: process.env.CLOUDINARY_API_SECRET,
+      secure: true,
+    })
+
+    let expression = "folder=snapi"
+    if (searchQuery) expression += ` AND tags=${searchQuery}`
+
+    const { resources } = await cloudinary.search.expression(expression).execute();
+
+    const resourceIds = resources.map((resource: any) => resource.public_id);
+
+    let query = {}
+
+    if (searchQuery) {
+      query = {
+        publicId: {
+          $in: resourceIds
+        }
+      }
+    }
+
+    const skipAmount = (Number(page) - 1) * limit;
+
+    const images = await populateUser(Image.find(query)).sort({ updatedAt: -1 }).skip(skipAmount).limit(limit);
+
+    const totalImages = await Image.find(query).countDocuments();
+    const savedImages = await Image.find().countDocuments()
+
+    return {
+      data: JSON.parse(JSON.stringify(images)),
+      totalPage: Math.ceil(totalImages / limit),
+      savedImages
+    }
   } catch (error) {
     handleError(error)
   }

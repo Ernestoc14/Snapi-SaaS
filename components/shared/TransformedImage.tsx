c259260ec41325ae45cc70bf34a69bcd468f7122
Ernestoc14 <descreed34@hotmail.com>
2024-05-28 12:21:45 -0500

    Feat: Update TransformedImage Component to include download functionality

--- components/shared/TransformedImage.tsx
+++ components/shared/TransformedImage.tsx
@@ -1,7 +1,7 @@
-"use client"
+"use client";
 
-import { dataUrl, debounce, getImageSize } from "@/lib/utils";
-import { CldImage } from "next-cloudinary";
+import { dataUrl, debounce, download, getImageSize } from "@/lib/utils";
+import { CldImage, getCldImageUrl } from "next-cloudinary";
 import { PlaceholderValue } from "next/dist/shared/lib/get-img-props";
 import Image from "next/image";
 
@@ -14,8 +14,22 @@ const TransformedImage = ({
   setIsTransforming,
   hasDownload = false,
 }: TransformedImageProps) => {
-  const downloadHandler = () => {};
-  
+  const downloadHandler = (
+    e: React.MouseEvent<HTMLButtonElement, MouseEvent>
+  ) => {
+    e.preventDefault();
+
+    download(
+      getCldImageUrl({
+        width: image?.width,
+        height: image?.height,
+        src: image?.publicId,
+        ...transformationConfig,
+      }),
+      title
+    );
+  };
+
   return (
     <div className="flex flex-col gap-4">
       <div className="flex-between">

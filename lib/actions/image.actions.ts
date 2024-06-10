1ca9b2639222c8fdee2c7baeb7c05df803564350
Ernestoc14 <descreed34@hotmail.com>
2024-06-10 13:50:56 -0500

    Feat: Updating Profile Page with User Images and Create a New SA to getUserImages

--- lib/actions/image.actions.ts
+++ lib/actions/image.actions.ts
@@ -126,3 +126,34 @@ export async function getAllImages({ page = 1, limit = 9, searchQuery = "" }: { 
     handleError(error)
   }
 }
+
+// GET USER IMAGES
+export async function getUserImages({
+  limit = 9,
+  page = 1,
+  userId,
+}: {
+  limit?: number;
+  page: number;
+  userId: string;
+}) {
+  try {
+    await connectToDatabase();
+
+    const skipAmount = (Number(page) - 1) * limit;
+
+    const images = await populateUser(Image.find({ author: userId }))
+      .sort({ updatedAt: -1 })
+      .skip(skipAmount)
+      .limit(limit);
+
+    const totalImages = await Image.find({ author: userId }).countDocuments();
+
+    return {
+      data: JSON.parse(JSON.stringify(images)),
+      totalPages: Math.ceil(totalImages / limit),
+    };
+  } catch (error) {
+    handleError(error);
+  }
+}
